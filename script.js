9ee101dde0931e450c6fc245cec20bbd22676ee9
FT Odin <fctanner@pm.me>
2025-04-18 09:00:14 +0200

    implement getcomputerchoice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const choices = ["Rock", "Paper", "Scissor"];
+
+getComputerChoice = function() {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const choice = choices[randomIndex];
+    return choice;
+};
+
+// console.log(getComputerChoice());
